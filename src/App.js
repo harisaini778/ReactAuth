fbdbf2d5b2037b51ecb2ec897865c9a77ac7190f
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-07-22 14:40:12 +0530

    Create a feature of auto logout.When a user logs in the token would remain active only for the next 5 minutes.After 5 minutes if the user refreshes the page he should be told to login again

--- src/App.js
+++ src/App.js
@@ -1,4 +1,39 @@
-import React from 'react';
+// import React from 'react';
+// import Home from './components/Home';
+// import LogIn from './components/LogIn';
+// import Profile from './components/Profile';
+// import './App.css';
+// import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+// import AuthContext, { AuthContextProvider } from './components/store/auth-context';
+// import { useContext } from 'react';
+
+// function App() {
+//   const AuthCtx = useContext(AuthContext);
+  
+//   return (
+//     <AuthContextProvider>
+//       <div>
+//         <Router>
+//           <Routes>
+//             {/* Public routes */}
+//             <Route path='/' element={<Home />} />
+//             <Route path='Home' element={<Home />} />;
+//             {!AuthCtx.isLoggedIn && <Route path='/LogIn' element={<LogIn />} />}
+
+//             {/* Private route */}
+//             {<Route path='/Profile' element={<Profile />} />}
+
+//             {/* Redirect to "/Home" if an unknown route is accessed */}
+//             <Route path='*' element={<Navigate to="/Home" />} />
+//           </Routes>
+//         </Router>
+//       </div>
+//     </AuthContextProvider>
+//   );
+// }
+// export default App;
+
+import React, { useState, useEffect } from 'react';
 import Home from './components/Home';
 import LogIn from './components/LogIn';
 import Profile from './components/Profile';
@@ -8,8 +43,41 @@ import AuthContext, { AuthContextProvider } from './components/store/auth-contex
 import { useContext } from 'react';
 
 function App() {
+  const [lastActivityTime, setLastActivityTime] = useState(Date.now());
   const AuthCtx = useContext(AuthContext);
-  
+
+  useEffect(() => {
+    // Check for inactivity every minute
+    const timer = setInterval(() => {
+      const currentTime = Date.now();
+      const timeSinceLastActivity = currentTime - lastActivityTime;
+      const inactivityDuration = 5 * 60 * 1000; // 5 minutes in milliseconds
+
+      // If more than 5 minutes of inactivity, logout the user
+      if (timeSinceLastActivity > inactivityDuration) {
+        // Clear the token and perform any other necessary logout actions
+        AuthCtx.logout();
+      }
+    }, 60000); // Check every minute
+
+    // Add event listeners to track user activity
+    const handleUserActivity = () => {
+      setLastActivityTime(Date.now());
+    };
+
+    document.addEventListener('click', handleUserActivity);
+    document.addEventListener('keydown', handleUserActivity);
+    // Add any other event listeners you want to track as user activity
+
+    // Clean up the timer and event listeners when the component unmounts
+    return () => {
+      clearInterval(timer);
+      document.removeEventListener('click', handleUserActivity);
+      document.removeEventListener('keydown', handleUserActivity);
+      // Remove any other event listeners here
+    };
+  }, [lastActivityTime, AuthCtx]);
+
   return (
     <AuthContextProvider>
       <div>
@@ -17,7 +85,7 @@ function App() {
           <Routes>
             {/* Public routes */}
             <Route path='/' element={<Home />} />
-            <Route path='Home' element={<Home />} />;
+            <Route path='/Home' element={<Home/>} />
             {!AuthCtx.isLoggedIn && <Route path='/LogIn' element={<LogIn />} />}
 
             {/* Private route */}
@@ -33,3 +101,4 @@ function App() {
 }
 
 export default App;
+
